5d2b69d39f2e89886a0ee1b1c14599e847029e22
Harry Mackenzie <Harry.Mackenzie@borwell.com>
2019-08-30 15:55:18 +0100

    Initial Commit

---
+++ BlazorStateTest/Features/Counter/CounterState.cs
@@ -0,0 +1,14 @@
+﻿using BlazorState;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStateTest.Features.Counter
+{
+    public partial class CounterState : State<CounterState>
+    {
+        public int Count { get; private set; }
+        protected override void Initialize() => Count = 3;
+    }
+}
